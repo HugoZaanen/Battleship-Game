fc35459acc9ac9254114cd008c5b88447a449b3c
Huego <hugozaanen@hotmail.com>
2018-09-18 15:03:28 +0200

    changes

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,62 +23,143 @@ namespace Battleship_Game
         Cell[] cellsPlayer = new Cell[4];
         Cell[] cellsComputer = new Cell[4];
 
-        bool gameStarted = false;
-        bool userTurn = false;
-        bool gameEnd = false;
-
-
-        int playerX;
-        int playerY;
+        int counter = 0;
+        int maxShip = 4;
         
+        bool gameStarted = false;
+        bool fieldIsSet = false;
+        bool playerTurn = false;
+        bool gameEnd = false;
+        bool playerIsSet = false;
+
+        //int playerX;
+        //int playerY;
+
         int pointX;
         int pointY;
+
+        Button[] playerButtons;
+            
+        Button[] buttons = new Button[4];
 
         public MainWindow()
         {
             InitializeComponent();
-
+            
             //int point = Button0_0.GetValue(Grid.RowProperty);
             Random rand = new Random(DateTime.Now.Millisecond);
             Random rand1 = new Random(DateTime.Now.Millisecond);
 
-            //
+            playerButtons = new Button[]
+                {
+                    buttonPlayer0_0,
+                    buttonPlayer1_0,
+                    buttonPlayer2_0,
+                    buttonPlayer3_0,
+                    buttonPlayer0_1,
+                    buttonPlayer1_1,
+                    buttonPlayer2_1,
+                    buttonPlayer3_1,
+                    buttonPlayer0_2,
+                    buttonPlayer1_2,
+                    buttonPlayer2_2,
+                    buttonPlayer3_2,
+                    buttonPlayer0_3,
+                    buttonPlayer1_3,
+                    buttonPlayer2_3,
+                    buttonPlayer3_3
+                };
+
             pointX = rand.Next(0,4);
             pointY = rand1.Next(0,4);
-            
+                       
         }
 
-        
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Button button = sender as Button;
+            if (playerIsSet)
+            {
+                while (playerTurn)
+                {
+                    if (playerTurn)
+                    {
+                        Button button = sender as Button;
 
-            int count = 0;
-            if ((int)button.GetValue(Grid.RowProperty) == pointX && (int)button.GetValue(Grid.ColumnProperty) == pointY)
-            {
-                button.Content = "Hit!";
-            }
-            else
-            {
-                button.Content = "Miss!" + count;
-                count++;
-            }
-            
-            while(button.Name.Substring(0,) == "")
-            {
-
+                        int count = 0;
+                        if ((int)button.GetValue(Grid.RowProperty) == pointX && (int)button.GetValue(Grid.ColumnProperty) == pointY)
+                        {
+                            button.Content = "Hit!";
+                            playerTurn = false;
+                        }
+                        else
+                        {
+                            button.Content = "Miss!";
+                            playerTurn = false;
+                        }
+                    }
+                    
+                }
             }
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void SelectButton(object sender, RoutedEventArgs e)
         {
-            Button button = sender as Button;
+            if (!playerIsSet)
+            {
+                Button button = sender as Button;
 
-            playerX = (int)button.GetValue(Grid.RowProperty);
-            playerY = (int)button.GetValue(Grid.ColumnProperty);
+                if (button.Content != "Ship")
+                {
+                    button.Content = "Ship";
+                    buttons[counter] = button;
+                    counter++;
+                }
 
-            MessageBox.Show("" + playerX);
-            MessageBox.Show("" + playerY);
+                if(counter == 4)
+                {
+                    playerIsSet = true;
+                }
+            }
+
+            if(playerIsSet)
+            {
+                gameStarted = true;
+                playerTurn = true;
+            }
+        }
+
+        private void StartButton(object sender, RoutedEventArgs e)
+        {          
+            gameStarted = true;
+        }
+
+        private void compTurn()
+        {
+            while (gameStarted)
+            {
+
+                if (!playerTurn)
+                {
+                    Random random = new Random();
+                    Random random1 = new Random();
+
+                    int i = random1.Next(0, 16);
+
+                    if (playerButtons[i].Content == "ship")
+                    {
+                        playerButtons[i].Content = "Hit!";
+                    }
+                    else
+                    {
+                        while (playerButtons[i].Content == "Miss")
+                        {
+                            i = random1.Next();
+                        }
+                        playerButtons[i].Content = "Miss";
+                    }
+                }
+                playerTurn = true;
+            }
         }
     }
 
